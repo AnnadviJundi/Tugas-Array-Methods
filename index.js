e88a24f4c6ccb9f103c53d72cefb2e0b012e35ed
Annadvi Jundi <144420937+AnnadviJundi@users.noreply.github.com>
2023-11-06 13:23:47 +0700

    Add files via upload
    Array Methods 
    POP,
    PUSH,
    SHIFT,
    UNSHIFT

---
+++ index.js
@@ -0,0 +1,39 @@
+const kota = [
+    "Jakarta",
+    "Surabaya",
+    "Malang",
+    "Bandung"
+]
+
+kota.pop();
+console.log(kota);
+
+const makanan = [
+    "Soto Ayam",
+    "Rawon",
+    "Sate",
+    "Bakso"
+]
+
+makanan.push("Mie Ayam");
+console.log(makanan);
+
+const game = [
+    "PUBG",
+    "Mobile Legends",
+    "Apex Legends",
+    "Clash of Clans"
+]
+
+game.shift();
+console.log(game);
+
+const aplikasi = [
+    "Facebook",
+    "Instagram",
+    "Twitter",
+    "Threads"
+]
+
+aplikasi.unshift("YouTube");
+console.log(aplikasi);
